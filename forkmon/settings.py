7fabb6913c3b3c56a27019c180109e1df247cc69
Andrew Chow <achow101-github@achow101.com>
2017-07-16 12:37:36 -0700

    Use Amazon RDS MySQL instead of sqlite

--- forkmon/settings.py
+++ forkmon/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'forkmon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
